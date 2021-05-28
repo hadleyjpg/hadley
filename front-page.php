347b015b31f550943fb22d1aa0d9dfbb3dea1d44
Hadley <hadleyslucher@gmail.com>
2021-05-28 16:01:54 -0400

    Front page design created

--- front-page.php
+++ front-page.php
@@ -11,9 +11,43 @@
  *
  * @package hadley
  */
-
-get_header();
 ?>
+	<section id="home" class="full"> 
+		<div class="home__text">
+			<?php the_custom_logo(); ?>
+			<h1>Hey, I'm Hadley</h1>
+			<h4>Developer, designer & professional plant killer</h4>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+			<p>Scroll down to learn more.</p>
+		</div>
+		<div class="home__caro">
+			<div class="caro">
+				<div class="caro__item">
+					<div class="caro__content">
+						<h2>Web Development</h2>
+						<p>Skilled in HTML, CSS, Javascript & PHP.</p>
+						<button class="elementor-button">View Work</button>
+					</div>
+				</div>
+				<div class="caro__item">
+					<div class="caro__content">
+						<h2>Web Design</h2>
+						<p>Skilled in HTML, CSS, Javascript & PHP.</p>
+						<button class="elementor-button">View Work</button>
+					</div>
+				</div>
+				<div class="caro__item">
+					<div class="caro__content">
+						<h2>Graphic Design</h2>
+						<p>Skilled in HTML, CSS, Javascript & PHP.</p>
+						<button class="elementor-button">View Work</button>
+					</div>
+				</div>
+			</div>
+		</div>
+    </section>
+
+	<?php get_header(); ?>
 
 	<main id="primary" class="site-main">
 
@@ -29,5 +63,7 @@ get_header();
 	</main><!-- #main -->
 
 <?php
+
+
 get_sidebar();
 get_footer();
